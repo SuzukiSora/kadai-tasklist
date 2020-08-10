bf928b53afdf34ea9716a7f6be45ebf98915eb8e
sora.suzuki <sorasuzuki1214@gmail.com>
2020-08-10 10:03:49 +0000

    post

--- app/helpers/toppages_helper.rb
+++ app/helpers/toppages_helper.rb
@@ -1,2 +1,8 @@
 module ToppagesHelper
+  def index
+    if logged_in?
+      @micropost = current_user.microposts.build  # form_with 用
+      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+    end
+  end
 end
