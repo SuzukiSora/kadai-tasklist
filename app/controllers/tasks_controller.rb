3e4640204484438ac9817d902753e4d666210336
sora.suzuki <sorasuzuki1214@gmail.com>
2020-08-12 12:01:14 +0000

    render

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,6 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:destroy, :show, :update, :edit]
   
   def index
-    @tasks = Task.all
     @task = current_user.tasks.build  # form_with 用
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
